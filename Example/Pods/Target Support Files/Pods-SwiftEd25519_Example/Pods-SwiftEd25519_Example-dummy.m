fbfa6ec7307bcae87ffdcf76d70ad401cf9e4575
HongYang Qiu <qiuhongyang@askcoin.org>
2017-08-28 12:39:01 +0800

    pod

---
+++ Example/Pods/Target Support Files/Pods-SwiftEd25519_Example/Pods-SwiftEd25519_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftEd25519_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftEd25519_Example
+@end
---
+++ Example/Pods/Target Support Files/SwiftEd25519/SwiftEd25519-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftEd25519 : NSObject
+@end
+@implementation PodsDummy_SwiftEd25519
+@end
